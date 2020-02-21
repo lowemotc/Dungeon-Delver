d708841d9e20c4cab09fee31dc648bd48db41c1c
lowem <lowem@otc.edu>
2020-02-21 10:51:29 -0600

    Dungeon Delver In-Class 1

---
+++ Assets/__Scripts/TileCamera.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+public class TileCamera : MonoBehaviour {
+    // Static Variables
+    static private int W, H;
+    static private int[,] MAP;
+    static public Sprite[] SPRITES;
+    static public Transform TILE_ANCHOR;
+    static public Tile[,] TILES;
+
+    [Header("Set In Inspector")]
+    public TextAsset mapData;
+    public Texture2D mapTiles;
+    public TextAsset mapCollisions;
+    public Tile tilePrefab;
+
+	// Use this for initialization
+	void Awake () {
+        LoadMap();
+	}
+
+    public void LoadMap()
+    {
+        // Create an anchor object
+        GameObject go = new GameObject("TILE_ANCHOR");
+        TILE_ANCHOR = go.transform;
+
+        SPRITES = Resources.LoadAll<Sprite>(mapTiles.name);
+
+        // Read in the map data
+        string[] lines = mapData.text.Split('\n');
+        H = lines.Length;
+        string[] tileNums = lines[0].Split(' ');
+        W = tileNums.Length;
+
+        System.Globalization.NumberStyles hexNum;
+        hexNum = System.Globalization.NumberStyles.HexNumber;
+
+        MAP = new int[W, H];
+        for (int j = 0; j < H; j++)
+        {
+            tileNums = lines[j].Split(' ');
+            for (int i = 0; i < W; i++)
+            {
+                print(tileNums[i]);
+                if (tileNums[i] == "..")
+                {
+                    MAP[i, j] = 0;
+                    print("Loaded 0");
+                }
+                else
+                {
+                    MAP[i, j] = int.Parse(tileNums[i], hexNum);
+                    print("Loaded " + tileNums[i]);
+                }
+            } // End of row for loop
+        } // End of column for loop
+        print("Parsed " + SPRITES.Length + " sprites");
+        print("Map size: " + W + " wide by " + H + " high");
+
+        ShowMap();
+    }
+
+    void ShowMap()
+    {
+        TILES = new Tile[W, H];
+        
+        for (int j = 0; j < H; j++)
+        {
+            for (int i = 0; i < W; i++){
+                if (MAP[i, j] != 0)
+                {
+                    Tile ti = Instantiate<Tile>(tilePrefab);
+                    ti.transform.SetParent(TILE_ANCHOR);
+                    ti.SetTile(i, j);
+                    TILES[i, j] = ti;
+                }
+            }
+        }
+    }
+
+    static public int GET_MAP(int x, int y)
+    {
+        if (x < 0 || x >= W || y < 0 || y >= H)
+        {
+            return -1;
+        }
+        return MAP[x, y];
+    }
+
+    static public int GET_MAP(float x, float y)
+    {
+        int tX = Mathf.RoundToInt(x);
+        int tY = Mathf.RoundToInt(y - 0.25f);
+        return GET_MAP(tX, tY);
+    }   // End of GET_MAP method
+	
+	static public void SET_MAP (int x, int y, int tNum) {
+		if (x < 0 || x >= W || y < 0 || y >= H)
+        {
+            return;
+        }
+        MAP[x, y] = tNum;               // Put this tile in the map array
+	}   // End of SET_MAP
+}
